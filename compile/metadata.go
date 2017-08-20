51281f947c9d878610fcd5d4a337eea6e78a61ca
Wyatt Arent <hello@wyatt.engineer>
2017-08-20 11:37:29 -0700

    Update meta compile

--- compile/metadata.go
+++ compile/metadata.go
@@ -1,12 +1,17 @@
 package compile
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/artificial-universe-maker/go-utilities/common"
 	"github.com/artificial-universe-maker/go-utilities/keynav"
 	"github.com/artificial-universe-maker/go-utilities/models"
 )
 
 func CompileMetadata(redisWriter chan common.RedisCommand, project models.AumProject) error {
-	redisWriter <- common.RedisSET(keynav.ProjectMetadataStaticProperty(project.ID, "title"), []byte(project.Title))
+	redisWriter <- common.RedisHSET(keynav.ProjectMetadataStatic(project.ID), "title", []byte(project.Title))
+	redisWriter <- common.RedisHSET(keynav.ProjectMetadataStatic(project.ID), "start_zone_id", []byte(fmt.Sprintf("%v", project.StartZone.Int64)))
+	redisWriter <- common.RedisHSET(keynav.GlobalMetaProjects(), strings.ToUpper(project.Title), []byte(fmt.Sprintf("%v", project.ID)))
 	return nil
 }
