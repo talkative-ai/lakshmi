52dcb870e7bd52ad94a09405f780a695ce84d939
Wyatt Arent <hello@wyatt.engineer>
2018-02-04 12:36:43 -0800

    Bugfix and refactor submit

--- routes/submit.go
+++ routes/submit.go
@@ -55,45 +55,51 @@ func postSubmitHandler(w http.ResponseWriter, r *http.Request) {
 			p."Title",
 			p."Category",
 			p."Tags",
-			json_agg(DISTINCT data) "ProjectData",
-			json_agg(DISTINCT triggers) "TriggerData"
-		FROM
-		(SELECT DISTINCT
-			za."ActorID",
-			za."ZoneID",
+			COALESCE((
+				SELECT jsonb_agg(data)
+				FROM (
+					SELECT DISTINCT
+						za."ActorID",
+						za."ZoneID",
 
-			d."ID" "DialogID",
-			d."EntryInput" "DialogEntry",
-			d."AlwaysExec",
-			d."Statements",
-			d."IsRoot",
-			d."UnknownHandler",
-			
-			dr."ParentNodeID" "ParentDialogID",
-			dr."ChildNodeID" "ChildDialogID"
+						d."ID" "DialogID",
+						d."EntryInput" "DialogEntry",
+						d."AlwaysExec",
+						d."Statements",
+						d."IsRoot",
+						d."UnknownHandler",
+						
+						dr."ParentNodeID" "ParentDialogID",
+						dr."ChildNodeID" "ChildDialogID"
 
-			FROM workbench_projects p
-			JOIN workbench_zones z
-				ON z."ProjectID" = p."ID"
-			JOIN workbench_zones_actors za
-				ON za."ZoneID"=z."ID"
-			JOIN workbench_dialog_nodes d
-				ON d."ActorID"=za."ActorID"
-			FULL OUTER JOIN workbench_dialog_nodes_relations dr
-				ON dr."ParentNodeID"=d."ID" OR dr."ChildNodeID"=d."ID"
-			WHERE p."ID"=$1) data,
-		(
-			SELECT DISTINCT
-				zone."ID" "ZoneID",
-				trig."TriggerType",
-				trig."AlwaysExec",
-				trig."Statements"
+						FROM workbench_projects p
+						JOIN workbench_zones z
+							ON z."ProjectID" = p."ID"
+						JOIN workbench_zones_actors za
+							ON za."ZoneID"=z."ID"
+						JOIN workbench_dialog_nodes d
+							ON d."ActorID"=za."ActorID"
+						FULL OUTER JOIN workbench_dialog_nodes_relations dr
+							ON dr."ParentNodeID"=d."ID" OR dr."ChildNodeID"=d."ID"
+						WHERE p."ID"=$1
+				) data
+			), '[]'::jsonb) AS "ProjectData",
+			COALESCE((
+				SELECT jsonb_agg(triggers)
+				FROM (
+					SELECT DISTINCT
+						zone."ID" "ZoneID",
+						trig."TriggerType",
+						trig."AlwaysExec",
+						trig."Statements"
 
-			FROM workbench_zones zone
-			JOIN workbench_triggers trig
-				ON trig."ZoneID"=zone."ID"
-			WHERE zone."ProjectID"=$1) triggers,
-		(
+					FROM workbench_zones zone
+					JOIN workbench_triggers trig
+						ON trig."ZoneID"=zone."ID"
+					WHERE zone."ProjectID"=$1
+				) triggers
+			), '[]'::jsonb) AS "TriggerData"
+		FROM (
 			SELECT
 				"Title",
 				"Category",
@@ -108,53 +114,59 @@ func postSubmitHandler(w http.ResponseWriter, r *http.Request) {
 	var currentVersion int64
 	status, _ := strconv.ParseInt(redis.Instance.Get(fmt.Sprintf("%v:%v", models.KeynavProjectMetadataStatic(projectID.String()), "status")).Val(), 10, 8)
 	currentPublishStatus := models.PublishStatus(status)
+	tx, err := db.Instance.Begin()
+	if err != nil {
+		common.RedisSET(
+			fmt.Sprintf("%v:%v", models.KeynavProjectMetadataStatic(projectID.String()), "status"),
+			[]byte(fmt.Sprintf("%v", models.PublishStatusProblem))).Exec(redis.Instance)
+		myerrors.ServerError(w, r, err)
+		return
+	}
 	if currentPublishStatus == models.PublishStatusPublishing {
 		return
 	} else if currentPublishStatus == models.PublishStatusDenied ||
 		currentPublishStatus == models.PublishStatusUnderReview {
 		currentVersion, err = redis.Instance.HGet(models.KeynavProjectMetadataStatic(projectID.String()), "version").Int64()
 		if err != nil {
+			common.RedisSET(
+				fmt.Sprintf("%v:%v", models.KeynavProjectMetadataStatic(projectID.String()), "status"),
+				[]byte(fmt.Sprintf("%v", models.PublishStatusProblem))).Exec(redis.Instance)
 			myerrors.ServerError(w, r, err)
 			return
 		}
-		db.Instance.Exec(`DELETE FROM static_published_projects_versioned WHERE "Version"=$1 AND "ProjectID"=$2`, currentVersion, projectID.String())
-		db.Instance.Exec(submitQuery, projectID.String(), currentVersion)
-		_, err = db.Instance.Exec(`INSERT INTO workbench_projects_needing_review ("ProjectID") VALUES ($1)`, projectID)
-		if err != nil {
-			myerrors.ServerError(w, r, err)
-			return
-		}
-		return
+		tx.Exec(`DELETE FROM static_published_projects_versioned WHERE "Version"=$1 AND "ProjectID"=$2`, currentVersion, projectID.String())
 	} else if currentPublishStatus == models.PublishStatusProblem {
-		// TODO: Handle this better
-		fmt.Println("There's an issue")
-		return
+		if err != nil {
+			common.RedisSET(
+				fmt.Sprintf("%v:%v", models.KeynavProjectMetadataStatic(projectID.String()), "status"),
+				[]byte(fmt.Sprintf("%v", models.PublishStatusProblem))).Exec(redis.Instance)
+			myerrors.ServerError(w, r, err)
+			return
+		}
+	} else {
+		currentVersion = redis.Instance.HIncrBy(models.KeynavProjectMetadataStatic(projectID.String()), "version", 1).Val()
+		trimVersions := currentVersion - 3
+		db.Instance.Exec(`DELETE FROM static_published_projects_versioned WHERE "Version"<=$1 AND "ProjectID"=$2`, trimVersions, projectID.String())
 	}
-
-	currentVersion = redis.Instance.HIncrBy(models.KeynavProjectMetadataStatic(projectID.String()), "version", 1).Val()
-	trimVersions := currentVersion - 3
-	db.Instance.Exec(`DELETE FROM static_published_projects_versioned WHERE "Version"<=$1 AND "ProjectID"=$2`, trimVersions, projectID.String())
 
 	common.RedisSET(
 		fmt.Sprintf("%v:%v", models.KeynavProjectMetadataStatic(projectID.String()), "status"),
 		[]byte(fmt.Sprintf("%v", models.PublishStatusPublishing))).Exec(redis.Instance)
-
-	db.Instance.Exec(submitQuery, projectID.String(), currentVersion)
-
-	// TODO: This should probably never need to run
-	_, err = db.Instance.Exec(`DELETE FROM workbench_projects_needing_review WHERE "ProjectID"=$1`, projectID)
+	tx.Exec(submitQuery, projectID.String(), currentVersion)
+	tx.Exec(`DELETE FROM workbench_projects_needing_review WHERE "ProjectID"=$1`, projectID)
+	tx.Exec(`INSERT INTO workbench_projects_needing_review ("ProjectID") VALUES ($1)`, projectID)
+	err = tx.Commit()
 	if err != nil {
+		common.RedisSET(
+			fmt.Sprintf("%v:%v", models.KeynavProjectMetadataStatic(projectID.String()), "status"),
+			[]byte(fmt.Sprintf("%v", models.PublishStatusProblem))).Exec(redis.Instance)
+		common.RedisSET(
+			fmt.Sprintf("%v:%v", models.KeynavProjectMetadataStatic(projectID.String()), "status"),
+			[]byte(fmt.Sprintf("%v", models.PublishStatusProblem))).Exec(redis.Instance)
 		myerrors.ServerError(w, r, err)
 		return
 	}
-	_, err = db.Instance.Exec(`INSERT INTO workbench_projects_needing_review ("ProjectID") VALUES ($1)`, projectID)
-	if err != nil {
-		myerrors.ServerError(w, r, err)
-		return
-	}
-
 	common.RedisSET(
 		fmt.Sprintf("%v:%v", models.KeynavProjectMetadataStatic(projectID.String()), "status"),
 		[]byte(fmt.Sprintf("%v", models.PublishStatusUnderReview))).Exec(redis.Instance)
-
 }
